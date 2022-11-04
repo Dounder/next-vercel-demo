6a318fb026321a8fe7300cc37ebd7407563d362a
dounder <dr.glasdou@gmail.com>
2022-11-04 10:47:47 -0600

    fix: pricing page

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -1,27 +1,28 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-import { HomeLayout } from '../../layouts/HomeLayout'
-import { LightLayout } from '../../layouts/LightLayout'
+import { HomeLayout } from '../../layouts/HomeLayout';
+import { LightLayout } from '../../layouts/LightLayout';
 
 export default function PricingPage() {
-	return (
-		<>
-			<h1>Pricing page</h1>
-			<h1 className={'title'}>
-				Go to <Link href="/">Home</Link>
-			</h1>
+  return (
+    <>
+      <h1>Pricing page</h1>
+      <h1 className={'title'}>
+        Go to <Link href="/">Home</Link>
+      </h1>
 
-			<p className={'description'}>
-				Get started by editing <code className={'code'}>pages/pricing/index.jsx</code>
-			</p>
-		</>
-	)
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/pricing/index.jsx</code>
+      </p>
+    </>
+  );
 }
 
-PricingPage.getLayout = function getLayout(page) {
-	return (
-		<HomeLayout title="Pricing - Dcode" description="Pricing page">
-			<LightLayout>{page}</LightLayout>
-		</HomeLayout>
-	)
-}
+PricingPage.getLayout = function getLayout(page: JSX.Element) {
+  return (
+    <HomeLayout title="Pricing - Dcode" description="Pricing page">
+      <LightLayout>{page}</LightLayout>
+    </HomeLayout>
+  );
+};
